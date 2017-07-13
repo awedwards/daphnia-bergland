f29ebadfe0313e5794f22b317c340e3c7ece89cb
Austin Edwards <edwardsa@janelia.hhmi.org>
2017-07-13 12:33:31 -0400

    added try/except statement around pedestal size code, added parse function to utils

--- bin/utils.py
+++ bin/utils.py
@@ -1,7 +1,7 @@
 import pickle
 import os
 import numpy as np
-import cv2
+import re
 
 def save_pkl(obj, path, name):
     with open(os.path.join(path, name) + '.pkl','wb') as f:
@@ -16,3 +16,22 @@ def merge_channels(im,channel1,channel2):
     copy[np.where(im[:,:,channel2])] = 1
     return copy
 
+def parse(s):
+    
+    # this method parses clone ids and finds pond name and id
+    #
+    # if only the pond is given (e.g. Cyrus), the id defaults to pond name
+
+    pattern = '^A?(?P<pond>[DW]>\s?\d{1,2}|Cyril|DBunk|Male|C14|Chard)[\s?|\.?|_?]?A?(?P<cloneid>\d{1,3})?A?'
+    m = re.search(pattern, s)
+    pond, cloneid = m.groups()
+
+    if cloneid is None:
+        cloneid = pond
+
+    if 'D' in pond and '8' in pond:
+        pond = 'D8'
+    elif 'D' in pond and '10' in pond:
+        pond = 'D10'
+
+    return pond, cloneid
