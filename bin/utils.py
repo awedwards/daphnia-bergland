fbbcbcf37a02bd217540df1ea0165650bb89d9df
Austin Edwards <edwardsa@janelia.hhmi.org>
2017-08-01 10:25:22 -0400

    added catch for different D8 pond assignment

--- bin/utils.py
+++ bin/utils.py
@@ -16,13 +16,13 @@ def merge_channels(im,channel1,channel2):
     copy[np.where(im[:,:,channel2])] = 1
     return copy
 
-def parse(s):
+def parsePond(s):
     
     # this method parses clone ids and finds pond name and id
     #
     # if only the pond is given (e.g. Cyrus), the id defaults to pond name
 
-    pattern = '^A?(?P<pond>[DW]>\s?\d{1,2}|Cyril|DBunk|Male|C14|Chard)[\s?|\.?|_?]?A?(?P<cloneid>\d{1,3})?A?'
+    pattern = '^A?([DW]?\s?\d{1,2}|Cyril|DBunk|Male|C14|Chard)[\s?|\.?|_?]?A?(\d{1,3})?A?'
     m = re.search(pattern, s)
     pond, cloneid = m.groups()
 
@@ -33,5 +33,17 @@ def parse(s):
         pond = 'D8'
     elif 'D' in pond and '10' in pond:
         pond = 'D10'
+    
+    elif pond == '8':
+        pond = 'D8'
 
     return pond, cloneid
+
+def parse(s):
+
+    pattern = '^([A-Za-z]{4,10})_(\d{6})?_?(DBunk_?\s?\d{1,3}|Male_\d|A?[DW]?\s?\d{1,2}[_|\s|.]?A?\d{1,3}A?|Cyril|C14|Chard)_(juju\d?|ctrl|NA|(?:\d*\.)?\d+)_(\d[A-Z]|NA)_(Rig[AB])_(\d{8}T\d{6}).bmp$'
+    
+    m = re.search(pattern,s)
+    filetype,barcode,cloneid,treatment,replicate,rigId,datetime = m.groups()
+
+    return filetype,barcode,cloneid,treatment,replicate,rigId,datetime
