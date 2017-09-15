9d62f11ea7c9902793da2cf1312ae74db5b15a2b
Austin Edwards <edwardsa@janelia.hhmi.org>
2017-09-15 11:35:42 -0400

    added df/dict/csv conversion functions to plot. probably going to move them to utils

--- bin/plot.py
+++ bin/plot.py
@@ -5,15 +5,20 @@ import pandas as pd
 import numpy as np
 from collections import defaultdict
 
-def dict_to_dataframe(clones):
+def dict_to_df(clones):
 
     data = defaultdict(list)
-
+    
     for bc in clones.iterkeys():
         for dtkey in clones[bc].iterkeys():
             for clone in clones[bc][dtkey].itervalues():
                 for k,v in clone.__dict__.iteritems():
                     data[k].append(v)
 
-   return pd.DataFrame(data = data)
+    return pd.DataFrame(data = data)
 
+def df_to_csv(df, path):   
+    df.to_csv(path, sep=',')
+
+def csv_to_df(path):
+    return pd.read_csv(path, sep=',')
