65a62ab257725f08c5bf0f9de4da263b75d059aa
Edwards <edwardsa@janelia.hhmi.org>
2017-10-16 13:04:46 -0400

    changed main to write to txt file after analysis

--- bin/main.py
+++ bin/main.py
@@ -3,7 +3,7 @@ from clone import Clone
 import utils
 import plot
 import os
-import pandas
+import pandas as pd
 import numpy as np
 import cv2
 from collections import default_dict
@@ -16,13 +16,16 @@ ANALYSISDIR = "/mnt/spicy_4/daphnia/analysis/"
 INDUCTIONMETADATADIR = "/mnt/spicy_4/daphnia/analysis/MetadataFiles/induction"
 ext = '.bmp'
 
-doAreaCalc = True
-doAnimalEllipseFit = True
-doEyeEllipseFit = True
-doBodyLandmarks = True
-doLength = False
-doOrientation = True
-doPedestalScore = False
+analysis = True
+
+if analysis == True:
+  doAreaCalc = True
+  doAnimalEllipseFit = True
+  doEyeEllipseFit = True
+  doBodyLandmarks = True
+  doLength = True
+  doOrientation = True
+  doPedestalScore = True
 
 files = os.listdir(DATADIR)
 
@@ -50,13 +53,12 @@ except IOError:
             data = wb["Inductions"].values
             cols = next(data)[0:]
             data = list(data)
-            df = pandas.DataFrame(data, columns=cols)
+	    df = pd.DataFrame(data, columns=cols)
             df = df[df.ID_Number.notnull()] 
             for j,row in df.iterrows():
                 if not row['ID_Number'] == "NaT":
                     inductiondates[row['ID_Number']] = row['Induction_Date']
-
-   
+ 
     # load manual_scales
     manual_scales = dict()
     with open(os.path.join(ANALYSISDIR, "manual_scales.txt"),'rb') as f:
@@ -97,63 +99,83 @@ except IOError:
 
     utils.save_pkl(clones, ANALYSISDIR, "clonedata")
 
-analysis = True
 so_far = 0
 
-def analyze(clone):
-    try:
-        im = cv2.imread(clone.full_filepath)
-        split = clone.split_channels(cv2.imread(clone.full_seg_filepath))
+with open(os.path.join(ANALYSISDIR, "daphnia_analysis_results.txt"), "wb") as f:
 
-        clone.calculate_area(split)
-        clone.fit_animal_ellipse(split)
-        clone.fit_ellipse(split,"eye", 4.6)
-        clone.find_body_landmarks(im, split)
-        clone.calculate_length()
-        print "Analyzed " + clone.full_filepath
-        return clone
-    except Exception as e:
-        print Exception
+    cols = ["filebase",
+    "barcode",
+    "cloneid",
+    "pond",
+    "id",
+    "season",
+    "treatment",
+    "replicate",
+    "rig",
+    "datetime",
+    "inductiondate",
+    "animal_area",
+    "animal_length"
+    "pixel_to_mm",
+    "animal_x_center",
+    "animal_y_center",
+    "animal_major",
+    "animal_minor",
+    "animal_theta",
+    "eye_x_center",
+    "eye_y_center",
+    "eye_major",
+    "eye_minor",
+    "eye_theta",
+    "anterior",
+    "posterior",
+    "dorsal",
+    "ventral",
+    "head",
+    "tail"]
+    
+    f.write( ",".join(cols) + "/n")
 
-with open("/home/austin/Documents/daphnia_analysis_log.txt", "wb") as f:
-    if analysis:
-        for barcode in clones.iterkeys():
-            for dt in clones[barcode].iterkeys():
-                for imtype in clones[barcode][dt].iterkeys():
-                    clone = clones[barcode][dt][imtype]
-                    print "Analyzing " + str(clone.filepath)
-                    try:
-                        split = clone.split_channels(cv2.imread(clone.seg_filepath))
-                        if doAreaCalc:
-                            print "Calculating area."
-                            clone.calculate_area(split)
-                        if doAnimalEllipseFit:
-                            print "Fitting ellipse to body."
-                            clone.fit_animal_ellipse(split)
-                        if doEyeEllipseFit:
-                            print "Fitting ellipse to eye."
-                            clone.fit_eye_ellipse(split)
-                        if doOrientation:
-                            print "Finding animal orientation."
-                            clone.get_anatomical_directions()
-                        if doBodyLandmarks and imtype == "full":
-                            print "Finding body landmarks."
-                            if clone.tail is None:
-                                im = cv2.imread(clone.filepath)
-                                clone.find_body_landmarks(im, split)
-                        if doLength:
-                            print "Calculating length."
-                            clone.calculate_length()
-                        if doPedestalScore:
-                            im = cv2.imread(clone.filepath)
-                            clone.get_pedestal_height(im,split)
-                            clone.calculate_pedestal_score()
-                    except AttributeError as e:
-                        print str(e)
-                        f.write("Error analyzing " + clone.filebase + " because: " + str(e) + "\n")
-                    so_far+=1
-
-                    if so_far%100==0:
-                        print "Saving " + str(so_far) + " out of many"
-                        utils.save_pkl(clones, ANALYSISDIR, "clonedata")
-        utils.save_pkl(clones,ANALYSISDIR, "clonedata")
+    for barcode in clones.iterkeys():
+        for dt in clones[barcode].iterkeys():
+	    clone = clones[barcode][dt]["full"]
+	    print "Analyzing " + str(clone.filepath)
+	    success = True
+	    try:
+		split = clone.split_channels(cv2.imread(clone.seg_filepath))
+		if doAreaCalc:
+		    print "Calculating area."
+		    clone.calculate_area(split)
+		if doAnimalEllipseFit:
+		    print "Fitting ellipse to body."
+		    clone.fit_animal_ellipse(split)
+		if doEyeEllipseFit:
+		    print "Fitting ellipse to eye."
+		    clone.fit_eye_ellipse(split)
+		if doOrientation:
+		    print "Finding animal orientation."
+		    clone.get_anatomical_directions()
+		if doBodyLandmarks and imtype == "full":
+		    print "Finding body landmarks."
+		    if clone.tail is None:
+			im = cv2.imread(clone.filepath)
+			clone.find_body_landmarks(im, split)
+		if doLength:
+		    print "Calculating length."
+		    clone.calculate_length()
+		#if doPedestalScore:
+		#    clone.initialize_snake()
+	    except AttributeError as e:
+		print str(e)
+		f.write("Error analyzing " + clone.filebase + " because: " + str(e) + "\n")
+		success = False
+            
+	    if success:
+	        tmpdata = list()
+	        for c in cols:
+		    tmpdata.append( getattr(clone, c) )
+	    
+	        f.write( ",".join(tmpdata) + "/n")
+	    
+	    if so_far%100==0:
+		print "Saving " + str(so_far) + " out of many"
